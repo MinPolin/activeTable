8b1a5d83c5558d1121c5ae913438d8d32513a101
Полина <vaganowa.p@yandex.ru>
2021-04-19 20:11:18 +0300

    Added files and changes required for deployment to heroku

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 from main import views
 urlpatterns = [
+    path('',RedirectView.as_view(url = '/admin/',permanent  =True)),
     path('admin/', admin.site.urls),
     path('data_store/', views.data_store),
     path('get_one/', views.get_one),
