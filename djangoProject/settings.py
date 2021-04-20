9bc70e4ac8f2e22e4057a91c5141c345012128e7
Полина <vaganowa.p@yandex.ru>
2021-04-20 18:29:25 +0300

    Added files and changes required for deployment to heroku

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -140,5 +140,7 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 STATIC_ROOT = 'staticfiles'
 import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
