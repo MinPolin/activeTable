e5d3e105a409dcb64ae45b7f4310321f0bcd75e3
Полина <vaganowa.p@yandex.ru>
2021-04-21 17:15:25 +0300

    Added files and changes required for deployment to heroku

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 from .models import Data
-admin.site.register(Data)
+
+
+class DataAdmin(admin.ModelAdmin):
+    list_display = ['base_id', 'status','fon_id' ]
+    list_filter = ['status']
+    search_fields = [field.name for field in Data._meta.fields]
+
+    class Meta:
+        model = Data
+
+
+admin.site.register(Data,DataAdmin)
